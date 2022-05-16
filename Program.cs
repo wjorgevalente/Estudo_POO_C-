fd9b3381fe7ec6ecf6a0d063582ee316a7cbbec7
william valente <wjorgevalente@gmail.com>
2022-05-16 15:01:26 -0300

    POO com c#

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Jogo_RPG_em_cSHARP.src.Entities;
+namespace Jogo_RPG_em_cSHARP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Knight arus = new Knight("Arus", 23, "Knight");
+          Wizard wizard = new Wizard("Jenica", 23, "White Wizard"); 
+          
+          Console.WriteLine(wizard.Attack(1));
+          Console.WriteLine(wizard.Attack(7));
+         
+        }
+    }
+}
---
+++ src/Entities/Wizard.cs
@@ -0,0 +1,25 @@
+namespace Jogo_RPG_em_cSHARP.src.Entities
+{
+  public class Wizard : Hero
+    {   
+        public Wizard(string Name, int Level, string HeroType)
+        {
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = HeroType;
+        }
+       public override string Attack(){
+            return this.Name + " Lançou magia";
+        } 
+
+        public string Attack(int Bonus){
+            if (Bonus > 6) {
+                return this.Name + " Lançou magia super efetiva com bonus de  " + Bonus;
+            } else
+            {
+                return this.Name + "Lançou uma magia com força fraca com bonus de " + Bonus;
+            }
+            
+        }
+    }
+}
